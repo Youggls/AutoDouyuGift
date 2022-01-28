853ed4456cf507e7d12f3b0e28508a3ec3537a72
Youggls <462046122@qq.com>
2022-01-28 17:01:33 +0800

    Fix bug.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ headers = {
 }
 
 
-def send_message_list(message_list: list(str), sc_key: str) -> None:
+def send_message_list(message_list: list, sc_key: str) -> None:
     raw_message = '\n'.join(message_list)
     requests.get(serve_base_url + sc_key + '.send?text=' + raw_message)
 
@@ -41,7 +41,7 @@ def format_cookie(cookie_str: str) -> dict:
     return cookie_dict
 
 
-def query_gift_list() -> list(dict):
+def query_gift_list() -> list:
     payload = {
         'rid': room_id,
     }
@@ -52,24 +52,30 @@ def query_gift_list() -> list(dict):
     return gift_list
 
 
-def send_gift(gift_list: list) -> list(str):
+def send_gift(gift_list: list) -> list:
     message_list = []
     send_gift_url = base_url + send_gift_uri
     for gift in gift_list:
-        gift_id = gift['giftId']
+        gift_id = gift['id']
         gift_num = gift['count']
         gift_name = gift['name']
         data = {
             'propId': gift_id,
-            'propCount': gift_num,
+            'propCount': 1,
             'roomId': room_id,
             'bizExt': '{"yzxq":{}}'
         }
-        res = requests.post(send_gift_url, data=data, headers=headers, cookies=format_cookie(cookie))
-        if res.status_code == 200:
-            message_list.append(f'赠送了 {gift_num} 个{gift_name}给房间: {room_id}')
-        else:
-            message_list.append(f'赠送 {gift_num} 个{gift_name}给房间: {room_id} 出现错误，错误代码为 {res.status_code}')
+        succ_cnt = 0
+        err_msg_list = []
+        for _ in range(0, gift_num):
+            res = requests.post(send_gift_url, data=data, headers=headers, cookies=format_cookie(cookie))
+            if res.status_code == 200 and res.json()['error'] == 0:
+                succ_cnt += 1
+            else:
+                err_msg_list.append(res.json())
+                message_list.append(f'赠送 {gift_num} 个{gift_name}给房间: {room_id} 出现错误，错误代码为 {res.status_code} : {res.json()}')
+        message_list.append(f'成功赠送了 {succ_cnt}/{gift_num} 个{gift_name}给房间: {room_id}')
+        message_list.append(f'赠送出现错误，共 {gift_num - succ_cnt}/{gift_num} 出现错误，错误信息为 {err_msg_list}')
     return message_list
 
 
@@ -77,5 +83,5 @@ if __name__ == '__main__':
     gift_list = query_gift_list()
     msg_list = send_gift(gift_list)
     print(msg_list)
-    if sc_key:
+    if sc_on:
         send_message_list(msg_list, sc_key)
